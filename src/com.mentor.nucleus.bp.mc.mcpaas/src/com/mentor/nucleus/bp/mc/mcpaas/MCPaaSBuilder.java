2249e547a5bc50d12138f63e80047f2ff06a280b
keithbrown <keith_brown@mentor.com>
2013-12-13 15:15:44 -0500

    job #130 - Implementation of new Java-based builder.  In a nutshell: - zips gen - http post's it to server - http get's built source code - unzips to src

---
+++ src/com.mentor.nucleus.bp.mc.mcpaas/src/com/mentor/nucleus/bp/mc/mcpaas/MCPaaSBuilder.java
@@ -0,0 +1,129 @@
+package com.mentor.nucleus.bp.mc.mcpaas;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.swt.widgets.Display;
+
+import com.mentor.nucleus.bp.core.util.HttpUtil;
+import com.mentor.nucleus.bp.core.util.UIUtil;
+import com.mentor.nucleus.bp.core.util.ZipUtil;
+import com.mentor.nucleus.bp.mc.AbstractActivator;
+
+public class MCPaaSBuilder extends IncrementalProjectBuilder {
+    
+	// The shared instance
+	private static MCPaaSBuilder singleton;
+
+	private static String mcpaasServer = "http://192.168.56.10:3000/model";    // TODO - need to pull this sever data from a preferences page
+	private static int MAX_GET_ITERATIONS = 6;  // TODO - get from preferences, or better yet consider another approach where the server sends a "done" message to this client when finished building
+	
+	public MCPaaSBuilder() {
+        super();
+	}
+
+	@Override
+    protected IProject[] build(int kind, Map args, IProgressMonitor monitor) {
+        UUID buildID = UUID.randomUUID();
+        String projDirStr = getProject().getLocation().toOSString();
+        File zipfile = new File(projDirStr + File.separator + buildID + ".zip");  // TODO - zip it up into the workspace/.metadata folder instead?
+        File genZipfile = new File(projDirStr + File.separator + "gen_" + buildID + ".zip");  // TODO - zip it up into the workspace/.metadata folder instead?
+        boolean success = true;
+        
+	    try {    
+	        // Zip up the gen/ folder
+	        IPath genDirPath = new Path(AbstractActivator.GEN_FOLDER_NAME + File.separator);
+	        IFolder genFolder = getProject().getFolder(genDirPath);
+	        genFolder.refreshLocal(IResource.DEPTH_ONE, null);
+	        if (genFolder.exists() && genFolder.members().length != 0) {
+	            File genDir = new File(projDirStr + File.separator + AbstractActivator.GEN_FOLDER_NAME);
+	            ZipUtil.zip(genDir, zipfile, true);	            
+	        }
+	        getProject().refreshLocal(IResource.DEPTH_INFINITE, null);
+        } catch (IOException ioe) {
+            UIUtil.openError("Could not package model data to send to build server: IOException.");
+            success = false;
+        } catch (CoreException ce) {
+            UIUtil.openError("Could not package model data to send to build server: CoreException.");
+            success = false;
+        }
+	        
+        // Send the zipfile to the MC-PaaS server
+	    if (success) {
+	        try {
+	            HttpUtil.postFile(mcpaasServer, "file", zipfile);
+	        } catch (Exception e) {
+	            UIUtil.openError("Could not send the file to the MC-PaaS server.");
+	            success = false;
+	        }
+	    }
+	        
+        // Request the source code zipfile from MC-PaaS server via HTTP GET loop
+	    if (success) {
+	        zipfile.renameTo(genZipfile);
+	        genZipfile.delete();
+	        boolean gotSrcFile = false;
+	        for (int i = 0; (i < MAX_GET_ITERATIONS) && !gotSrcFile; i++) {
+	            try {
+                    Thread.sleep(10000);// TODO - change back up and to 10s
+	                HttpUtil.getFile(mcpaasServer + "/" + buildID + ".zip", zipfile.getCanonicalPath());
+	                if (zipfile.exists()) {
+	                    gotSrcFile = true;
+	                }
+	            } catch (Exception e) {
+	                // A failure to get is normal if the build is still in process
+	            }
+	        }
+	        if (!gotSrcFile) {
+	            success = false;
+	            UIUtil.openError("Timed out waiting for source code packet from MC-PaaS server.");
+	        }
+	    }
+
+	    if (success) {
+	        try {
+	            // Unzip the build file to the src/ folder
+	            zipfile = new File(projDirStr + File.separator + buildID + ".zip");
+	            ZipUtil.unzip(zipfile, new File(projDirStr));
+	            getProject().refreshLocal(IResource.DEPTH_INFINITE, null);
+	        } catch (IOException ioe) {
+	            UIUtil.openError("Could not unzip model data from the build server: IOException.");
+	            success = false;
+	        } catch (CoreException ce) {
+	            UIUtil.openError("Could not package model data to send to build server: CoreException.");
+	            success = false;
+	        }
+	    }
+	    
+        // Clean up
+	    try {
+	        success = zipfile.delete();
+	        getProject().refreshLocal(IResource.DEPTH_INFINITE, null);
+	    } catch (CoreException ce) {}
+	    
+        return null;
+    }
+	
+	/**
+	 * Returns the shared instance. Creates if it has not yet been created
+	 * 
+	 * @return the shared instance
+	 */
+	public static MCPaaSBuilder getDefault() {
+		if (MCPaaSBuilder.singleton == null) {
+			MCPaaSBuilder.singleton = new MCPaaSBuilder();
+		}
+		return MCPaaSBuilder.singleton;
+	}
+	
+}
